fa058850245e5b13cac7cbfd4c5c4816cdd30e3d
817677 <817677@605-110-S02>
2023-02-27 14:45:13 -0700

    Add project files.

---
+++ quizz/quizz.py
@@ -0,0 +1,12 @@
+baguette = 0
+breadstick = 0
+white = 0
+pita = 0
+
+print("First things first, pick a shirt: (b)asic tee, (v)intage print, (l)acy blouse, (f)lowy crop.")
+answer = input()
+if answer == 'b':
+    white+=1
+elif answer == 'v':
+    baguette+=1
+elif answer == 'l':
